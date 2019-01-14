cee59c1a28060c7dc7438510b83f28df49ee8306
John Ankarström <john@ankarstrom.se>
2019-01-15 00:17:37 +0100

    ttyfd and basic selection returning

--- browse.c
+++ browse.c
@@ -5,15 +5,36 @@
 #include <err.h>
 #include <sys/types.h> /* DIR */
 #include <dirent.h> /* opendir */
+#include <fcntl.h> /* open */
 
-char *program_name;
+/**********************************************************************\
+|                                                                      |
+|  browse(1) is a terminal-based directory browser and selector.       |
+|                                                                      |
+|  The source code is divided into two separate parts:                 |
+|                                                                      |
+|  1. directory reading                                                |
+|  2. directory displaying                                             |
+|                                                                      |
+|  The directory display is written *not* to the standard output,      |
+|  but to the tty's file descriptor.                                   |
+|                                                                      |
+|  Only the final selection is to be printed on the standard output.   |
+|                                                                      |
+\**********************************************************************/
 
-/** options **/
+/** command-line options **/
 bool option_all = false;
 
 /** terminal **/
+int ttyfd;
 size_t screen_rows = 20;
 size_t screen_rows_free = 4;
+
+/** program **/
+char *program_name;
+
+/*** helper functions ***/
 
 void die_usage() {
 	fprintf(stderr, "usage: %s [-a]\n", program_name);
@@ -33,7 +54,7 @@ void display(size_t count, struct dirent *entries[]) {
 void count_dir(DIR *stream, size_t *all, size_t *visible) {
 	*all = 0;
 	*visible = 0;
-	
+
 	long location;
 	if ((location = telldir(stream)) == -1) err(1, "telldir");
 	rewinddir(stream);
@@ -51,7 +72,7 @@ void count_dir(DIR *stream, size_t *all, size_t *visible) {
 	seekdir(stream, location); /* reset */
 }
 
-void browse(char *dirname) {
+char *browse(char *dirname) {
 	/* count entries */
 	DIR *stream;
 	size_t all;
@@ -78,15 +99,31 @@ void browse(char *dirname) {
 
 	/* display entries */
 	display(count, entries);
+
+	/* return selection */
+	size_t size = strlen(entries[0]->d_name) + 1; /* placeholder */
+	char *selection = malloc(size);
+	strlcpy(selection, entries[0]->d_name, size);
+
+	if (closedir(stream) == -1) err(1, "closedir");
+
+	return selection;
 }
+
+/*** command-line interface ***/
 
 int main(int argc, char *argv[]) {
 	program_name = argv[0];
+	if ((ttyfd = open("/dev/tty", O_RDWR)) == -1) err(1, "open");
+
 	if (argc > 2) die_usage();
 	if (argc == 2) {
 		if (strcmp(argv[1], "-a") == 0) option_all = true;
 		else die_usage();
 	}
 
-	browse(".");
+	char *selection = browse(".");
+	printf("%s\n", selection);
+	return 0;
+	/* assume that the kernel frees ttyfd */
 }
